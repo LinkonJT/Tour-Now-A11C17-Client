fe87cf9edb01c757d6ce1ac1e88cc277ca626685
LinkonJT <linkontripura@gmail.com>
2025-06-16 13:45:11 +0600

    404 page setup done with animation

--- src/pages/NotFound404.jsx
+++ src/pages/NotFound404.jsx
@@ -1,9 +1,14 @@
+import Lottie from 'lottie-react';
 import React from 'react';
+import { NavLink } from 'react-router';
+import notFoundLottie from '../assets/lottie/NotFound404.json'
 
 const NotFound404 = () => {
     return (
-        <div>
-            404 Not Found
+        <div className='mx-auto w-11/12 text-center'>
+            <Lottie className='w-120 mx-auto' animationData={notFoundLottie} loop={true}></Lottie>
+            <p> "Lost your way? Even the best explorers sometimes wander off the map."</p>
+            <NavLink to={'/'} className="btn"> Back to Home</NavLink>
         </div>
     );
 };
